5e90c5b03ec7219bd6a8514e1ced9e59a760218f
“Reese <reesegreen2014@me.com>
2024-07-29 11:05:40 -0600

    adds search functionality to all breeds page and implements load handling

--- src/components/AllBreeds/AllBreeds.js
+++ src/components/AllBreeds/AllBreeds.js
@@ -5,29 +5,53 @@ import './AllBreeds.css';
 
 function AllBreeds() {
   const [breeds, setBreeds] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [searchTerm, setSearchTerm] = useState('');
 
   useEffect(() => {
     fetchAllBreeds().then(data => {
       console.log('Fetched Breeds:', data);
       setBreeds(data);
+      setLoading(false);
     });
   }, []);
+
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const filteredBreeds = breeds.filter(breed =>
+    breed.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
   return (
     <div className='all-breeds-container'>
       <h1>All Breeds</h1>
-      <div className='breeds-grid'>
-        {breeds.map((breed) => (
-          <Link to={`/breed/${breed.id}`} key={breed.id} className='breed-card-link'>
-            <div className='breed-card'>
-              {breed.reference_image_id && (
-                <img src={`https://cdn2.thedogapi.com/images/${breed.reference_image_id}.jpg`} alt={breed.name} />
-              )}
-              <h2>{breed.name}</h2>
-            </div>
-          </Link>
-        ))}
-      </div>
+      <input
+        type="text"
+        placeholder="Search by breed name"
+        value={searchTerm}
+        onChange={handleSearchChange}
+        className="search-bar"
+      />
+      {loading ? (
+        <div className="spinner-container">
+          <div className="spinner"></div>
+        </div>
+      ) : (
+        <div className='breeds-grid'>
+          {filteredBreeds.map((breed) => (
+            <Link to={`/breed/${breed.id}`} key={breed.id} className='breed-card-link'>
+              <div className='breed-card'>
+                {breed.reference_image_id && (
+                  <img src={`https://cdn2.thedogapi.com/images/${breed.reference_image_id}.jpg`} alt={breed.name} />
+                )}
+                <h2>{breed.name}</h2>
+              </div>
+            </Link>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
