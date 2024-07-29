feaff0967f12656d575ca47cfc8f08bab733cd1a
“Reese <reesegreen2014@me.com>
2024-07-29 10:18:12 -0600

    initial styling and error handling of missing information

--- src/components/BreedDetails/BreedDetails.js
+++ src/components/BreedDetails/BreedDetails.js
@@ -1,36 +1,83 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { fetchBreedDetails } from '../../ApiCalls/apiCalls';
-import './BreedDetails.css'
-import { Link } from 'react-router-dom';
+import './BreedDetails.css';
 
 function BreedDetails() {
-
-  const {id} = useParams();
+  const { id } = useParams();
   const [breedDetails, setBreedDetails] = useState(null);
 
   useEffect(() => {
     fetchBreedDetails(id).then(data => {
       console.log('Breed details:', data);
       setBreedDetails(data);
-    })
-  }, [])
+    });
+  }, [id]);
 
   if (!breedDetails) {
-    return <div>Loading...</div>
+    return <div>Loading...</div>;
   }
-  
+
   return (
-    <div>
-      {breedDetails.image && <img src={breedDetails.image.url} alt={breedDetails.name} />}
-      <h1>{breedDetails.name}</h1>
-      <p>Did you know? The {breedDetails.name} originates from {breedDetails.origin}.</p>
-      <p>Bred for: {breedDetails.bred_for}</p>
-      <p>Breed group: {breedDetails.breed_group}</p>
-      <p>Height: {breedDetails.height.imperial} inches</p>
-      <p>Weight: The {breedDetails.name} weighs on average between {breedDetails.weight.imperial} lbs.</p>
-      <p>The {breedDetails.name} lives on average between {breedDetails.life_span} years</p>
-      <p>Some of the characteristics that a {breedDetails.name} is known for are: {breedDetails.temperament}</p>
+    <div className='breed-details-page'>
+      {breedDetails.image && <img src={breedDetails.image.url} alt={breedDetails.name} className='breed-image' />}
+      <h1 className='dog-name'>{breedDetails.name}</h1>
+      <div className='dog-info'>
+        <div className='card'>
+          <h3>Did you know?</h3>
+          <p>
+            {breedDetails.origin 
+              ? `The ${breedDetails.name} originates from ${breedDetails.origin}.`
+              : "We're working on getting that information! Check back soon."
+            }
+          </p>
+        </div>
+        <div className='card'>
+          <h3>The {breedDetails.name} was bred for</h3>
+          <p>
+            {breedDetails.bred_for 
+              ? breedDetails.bred_for 
+              : "We're working on getting that information! Check back soon."
+            }
+          </p>
+        </div>
+        <div className='card'>
+          <h3>Breed Group</h3>
+          <p>
+            {breedDetails.breed_group 
+              ? breedDetails.breed_group 
+              : "We're working on getting that information! Check back soon."
+            }
+          </p>
+        </div>
+        <div className='card'>
+          <h3>Height & Weight</h3>
+          <p>
+            {(breedDetails.height?.imperial || breedDetails.weight?.imperial) 
+              ? `${breedDetails.height?.imperial ? `The ${breedDetails.name} has an average height of ${breedDetails.height.imperial} inches` : ''}${breedDetails.height?.imperial && breedDetails.weight?.imperial ? ' and ' : ''}${breedDetails.weight?.imperial ? `generally weighs between ${breedDetails.weight.imperial} lbs.` : ''}`
+              : "We're working on getting that information! Check back soon."
+            }
+          </p>
+        </div>
+        <div className='card'>
+          <h3>Lifespan</h3>
+          <p>
+            {breedDetails.life_span 
+              ? `The ${breedDetails.name} lives on average between ${breedDetails.life_span} years.` 
+              : "We're working on getting that information! Check back soon."
+            }
+          </p>
+        </div>
+        <div className='card'>
+          <h3>Unique characteristics</h3>
+          <p>
+            {breedDetails.temperament 
+              ? `Some of the characteristics that a ${breedDetails.name} is known for are: ${breedDetails.temperament}`
+              : "We're working on getting that information! Check back soon."
+            }
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
