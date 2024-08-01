511b14ab9f14921fd24a884cf94f54ef50a7f7e9
“Reese <reesegreen2014@me.com>
2024-08-01 12:39:52 -0600

    error handling 404

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -6,7 +6,7 @@ import lost from '../../images/Lost Dog.jpg';
 const NotFound = () => {
   return (
     <div className="not-found-container">
-        <img src={lost}></img>
+        <img src={lost} alt='lost dog in field'></img>
       <h1>404</h1>
       <p>Page Not Found</p>
       <Link to="/">Go Back Home</Link>
